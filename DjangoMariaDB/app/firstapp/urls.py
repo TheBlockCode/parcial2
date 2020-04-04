b0dd37bf176e3268449ad65d0e8845f16e70723f
TheBlockCode <jesussm_0001@hotmail.com>
2020-04-03 21:30:31 -0500

    parcial 2 parte 2

--- DjangoMariaDB/app/firstapp/urls.py
+++ DjangoMariaDB/app/firstapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('client/login',views.login,name='login'),
+    path('client/list',views.showList,name='showList'),
     path('generate_password/<str:password>',views.makepassword,name='makepassword')
 ]
--- DjangoMariaDB/app/firstapp/views.py
+++ DjangoMariaDB/app/firstapp/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 #IMPORT models
 from .models import Movie,ApiUsers
+from django.forms.models import model_to_dict
 
 #IMPORT LIBRARIRES/FUNCTIONS
 #from django.shortcuts import render , HttpResponse
@@ -12,32 +13,137 @@ from django.contrib.auth.hashers import make_password, check_password
 
 #check_password(noHashPassword,HashedPassword) this funcion validate if the password match to the hash
 
+def showList(request):
+	#VALIDATE METHOD
+	if request.method == 'GET':
+		#DECLARE RESPONSE
+		responseData = {}
+
+		#CHECK JSON STRUCTURE
+		isJson = checkJson()
+		isJsonResult = isJson.isJson(request.body)
+
+		if (type(isJsonResult) == type(True)):
+			jsonBody = json.loads(request.body)
+			#CHECK JSON CONTENT
+			if 'user' not in jsonBody:
+				responseData['result'] = 'error'
+				responseData['message'] = 'user is required'
+				return JsonResponse(responseData, status=401)
+
+			if 'password' not in jsonBody:
+				responseData['result'] = 'error'
+				responseData['message'] = 'password is required'
+				return JsonResponse(responseData, status=401)
+
+			#CHECK IF USER EXITST
+			if (ApiUsers.objects.get(user=jsonBody['user']) != None):
+				#TAKE PASSWORD OF THE USER
+				user = ApiUsers.objects.get(user=jsonBody['user'])
+
+				#CHECK IF PASSWORD IS CORRECT
+				if check_password(jsonBody['password'], user.password):
+					#CHECK IF USER HAS API-KEy
+					if ApiKey().check(request) == True:
+						if user.api_key == request.headers["user-api-key"]:
+							#pelis= Movie.objects.all()
+							count = 0
+							#RETURN RESPONSE
+							responseData['result'] = 'success'
+							responseData['message'] = 'Valid Credentials'
+							responseData['movies'] = {}
+
+							for i in Movie.objects.all():
+								responseData["movies"][count]={}
+								responseData["movies"][count]['id']= i.movieid
+								responseData["movies"][count]['title']= i.movietitle
+								responseData["movies"][count]['releaseDate']= i.releasedate
+								responseData["movies"][count]['imageurl']= i.imageurl
+								count = count + 1
+							return JsonResponse(responseData,status=200)
+						else:
+							responseData['result'] = 'error'
+							responseData['message'] = 'Api key incorrecto'
+							return JsonResponse(responseData, status=401)
+
+					else:
+						responseData['result'] = 'error'
+						responseData['message'] = 'se requiere api key'
+						return JsonResponse(responseData, status=401)
+
+
+				else:
+                    			responseData['result'] = 'error'
+                    			responseData['message'] = 'The user does not exist or the password is incorrect'
+                    			return JsonResponse(responseData, status=401)
+
+			else:
+				responseData['result'] = 'error'
+				responseData['message'] = 'The user does not exist or the password is incorrect'
+				return JsonResponse(responseData, status=401)
+
+		else:
+			return isJsonResult
+
+	else:
+		responseData = {}
+		responseData['result'] = 'error'
+		responseData['message'] = 'Invalid Request'
+		return JsonResponse(responseData, status=400)
+
 def login(request):
 
 	#VALIDATE METHOD
 	if request.method == 'POST':
+		#DECLARE RESPONSE
+		responseData = {}
 
-		#DECLARE RESPONS
-		if checkJson().isJson(request.body)==True:
-			response_data={}
-			json_data = json.loads(request.body)
-			attr_error = False
-			attrErrorMsg = ""
-			if 'usuario' not in json_data:
-				attr_error = True
-				attrErrorMsg = "Llene el campo"
-			elif 'contraseña' not in json_data:
-				attr_error = True
-				attrErrorMsg="Llene el campo"
-			if attr_error == True:
-			
-				response_data['result'] = 'error'
-				response_data['message'] = attrErrorMsg
-				return JsonResponse(response_data, status=401)
+		#CHECK JSON STRUCTURE
+		isJson = checkJson()
+		isJsonResult = isJson.isJson(request.body)
+
+		if (type(isJsonResult) == type(True)):
+			jsonBody = json.loads(request.body)
+			#CHECK JSON CONTENT
+			if 'user' not in jsonBody:
+				responseData['result'] = 'error'
+				responseData['message'] = 'user is required'
+				return JsonResponse(responseData, status=401)
+
+			if 'password' not in jsonBody:
+				responseData['result'] = 'error'
+				responseData['message'] = 'password is required'
+				return JsonResponse(responseData, status=401)
+
+			#CHECK IF USER EXITST
+			if (ApiUsers.objects.get(user=jsonBody['user']) != None):
+				#TAKE PASSWORD OF THE USER
+				user = ApiUsers.objects.get(user=jsonBody['user'])
+
+				#CHECK IF PASSWORD IS CORRECT
+				if check_password(jsonBody['password'], user.password):
+					#CHECK IF USER HAS API-KEY
+					if user.api_key == None:
+						user.api_key = ApiKey().generate_key_complex()
+						user.save()
+					#RETURN RESPONSE
+					responseData['result'] = 'success'
+					responseData['message'] = 'Valid Credentials'
+					responseData['userApiKey'] = user.api_key
+					return JsonResponse(responseData, status=200)
+
+				else:
+                    			responseData['result'] = 'error'
+                    			responseData['message'] = 'The user does not exist or the password is incorrect'
+                    			return JsonResponse(responseData, status=401)
+
 			else:
-				response_data['result'] = 'success'
-				response_data['message'] = 'Correcto'
-				return JsonResponse(response_data, status=200)
+				responseData['result'] = 'error'
+				responseData['message'] = 'The user does not exist or the password is incorrect'
+				return JsonResponse(responseData, status=401)
+
+		else:
+			return isJsonResult
 
 	else:
 		responseData = {}
